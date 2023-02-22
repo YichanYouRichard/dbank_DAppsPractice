cf2cfd2d52a2204013e446bd5eca55c77fccb1a1
Richard You <richardyou94@gmail.com>
2023-02-22 12:41:08 -0800

    checkbalance function works in frontend js

---
+++ src/dbank_frontend/src/index.js
@@ -0,0 +1,8 @@
+import { dbank_backend } from "../../declarations/dbank_backend";
+
+window.addEventListener("load", async function () {
+  
+  const currentAmount = await dbank_backend.checkBalance();
+
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+});
