4df59e2fb89a9aba8319c195d4dfcb27874cc43e
Richard You <richardyou94@gmail.com>
2023-02-22 12:49:16 -0800

    add frontend js for parsefloat the input number,

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,8 +1,28 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
 
 window.addEventListener("load", async function () {
-  
   const currentAmount = await dbank_backend.checkBalance();
 
-  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+  document.getElementById("value").innerText =
+    Math.round(currentAmount * 100) / 100;
 });
+
+document
+  .querySelector("form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const inputAmount = parseFloat(
+      document.getElementById("input-amount").value
+    );
+    const outputAmount = parseFloat(
+      document.getElementById("withdrawal-amount").value
+    );
+
+    await dbank_backend.topUp(inputAmount);
+    await dbank_backend.withdrawal(outputAmount);
+
+    const currentAmount = await dbank_backend.checkBalance();
+    document.getElementById("value").innerText =
+      Math.round(currentAmount * 100) / 100;
+  });
